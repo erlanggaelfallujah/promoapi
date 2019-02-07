576f7bc95e1c8bd43945a1b7925dfdfa83648582
Erlangga <rangga_muslim@yahoo.co.id>
2019-02-07 19:22:02 +0700

    promo success test case

--- src/test/java/dev/ranggalabs/promo/service/PromoServiceTest.java
+++ src/test/java/dev/ranggalabs/promo/service/PromoServiceTest.java
@@ -4,10 +4,6 @@ import dev.ranggalabs.promo.Application;
 import dev.ranggalabs.promo.dto.PromoRequestDto;
 import dev.ranggalabs.promo.dto.PromoResponseDto;
 import io.reactivex.Single;
-import io.reactivex.SingleObserver;
-import io.reactivex.disposables.Disposable;
-import io.reactivex.functions.Consumer;
-import io.reactivex.subscribers.TestSubscriber;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -16,7 +12,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.Date;
-import java.util.concurrent.TimeUnit;
+import java.util.Random;
 
 import static org.hamcrest.core.Is.is;
 
@@ -32,15 +28,21 @@ public class PromoServiceTest {
 
     @Test
     public void testNormalPromoService(){
-        PromoRequestDto dto = new PromoRequestDto();
-        dto.setAmount(1500D);
-        dto.setMid("mid_test_found");
-        dto.setTid("tid_test_found");
-        dto.setTransactionDate(new Date());
+        for(int i=0;i<100;i++){
+            PromoRequestDto dto = new PromoRequestDto();
+            dto.setAmount(new Double(getRandomNumberInRange(1000,2000)));
+            dto.setMid("mid_test_found");
+            dto.setTid("tid_test_found");
+            dto.setTransactionDate(new Date());
 
-        Single<PromoResponseDto> response = promoService.process(dto);
-        PromoResponseDto responseDto = response.blockingGet();
-        Assert.assertTrue(responseDto.getResponseCode().equals("00"));
+            Single<PromoResponseDto> response = promoService.process(dto);
+            PromoResponseDto responseDto = response.blockingGet();
+            Assert.assertThat(responseDto.getResponseCode(),is("00"));
+            Assert.assertThat(responseDto.getRewards().size(),is(1));
+            Assert.assertThat(responseDto.getRewards().get(0).getResponseCode(),is("00"));
+            Assert.assertThat(responseDto.getRewards().get(0).getDiscount().getName(),is("test_discount_5%"));
+            Assert.assertThat(responseDto.getNetAmount(),is((dto.getAmount()-((dto.getAmount()*responseDto.getRewards().get(0).getDiscount().getPercentage())/100))));
+        }
     }
 
     @Test
@@ -56,5 +58,15 @@ public class PromoServiceTest {
         Assert.assertThat(promoResponseDto.getResponseCode(),is("A1"));
         Assert.assertThat(promoResponseDto.getNetAmount(),is(dto.getAmount()));
     }
+
+    private int getRandomNumberInRange(int min, int max) {
+        if (min >= max) {
+            throw new IllegalArgumentException("max must be greater than min");
+        }
+
+        Random r = new Random();
+        return r.nextInt((max - min) + 1) + min;
+    }
+
 
 }
--- src/test/resources/data.sql
+++ src/test/resources/data.sql
@@ -1,3 +1,3 @@
-INSERT INTO campaign(name,mid,tid,start_date,end_date,min_amount,max_amount) VALUES ('test_campaign_found','mid_test_found','tid_test_found',CURRENT_DATE(),CURRENT_DATE(),1000,2000);
+INSERT INTO campaign(name,mid,tid,start_date,end_date,min_amount,max_amount,reward_type) VALUES ('test_campaign_found','mid_test_found','tid_test_found',CURRENT_DATE(),CURRENT_DATE(),1000,2000,1);
 INSERT INTO discount VALUES(1,'test_discount_5%',5);
 INSERT INTO promo_map VALUES(1,1);
